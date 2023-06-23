3d224bf9041e581525d8b1a56a8adc8f7c355a6a
devgeniegenie <devgeniegenie@gmail.com>
2023-06-23 13:51:12 +0900

    로그인 정보 파일 분리

--- autoGrpwareCheckTest.js
+++ autoGrpwareCheckTest.js
@@ -1,22 +1,32 @@
 const playwright = require('playwright');
 const {chromium} = require("playwright");
+const {gwId, gwPassword} = require('./var');
 
 (async () => {
     const browser = await chromium.launch({
-        headless: false
+        headless: false,
+        fullscreen: true
     });
     const context = await browser.newContext();
     const page = await context.newPage();
 
     await page.goto('https://my.naonsoft.com/ekp/view/login/userLogin');
     await page.click('#userId');
-    await page.fill('#userId', 'myId');
+    await page.fill('#userId', gwId);
     await page.locator('#login_loginBox div').click();
-    await page.fill('#password', 'myPassword');
+    await page.fill('#password', gwPassword);
     await page.click('#btnLogin');
-    const parentElement = await page.waitForSelector('#lnb');
-    const childElement = await parentElement.waitForSelector('a[title="????"]');
+    // const parentElement = await page.waitForSelector('#lnb');
 
-    await childElement.click();
-    await browser.close();
+    // await childElement.click();
+    // await browser.close();
+    const timeout = 5000; // ?? ?? (???)
+
+    await page.waitForFunction(() => {
+        const element = document.querySelectorAll('a[title="????"]');
+        return element !== null
+    }, {timeout: timeout});
+
+    await page.click($('a[title="????"]'));
+
 })();
